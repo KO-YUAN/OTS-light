28d70d9d29b602128e039f622b50e126b1dfc33c
junsiang <junsiangchen@himalayasoft.xyz>
2025-05-20 17:45:23 +0800

    feat: 修改首頁路徑

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,15 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/zh-TW',
+        permanent: true,
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+export default nextConfig; 
